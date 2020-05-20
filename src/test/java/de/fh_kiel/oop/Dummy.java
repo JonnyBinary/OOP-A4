e439ca94d55b8051ac03eec3c2be020fea67e6c3
Jonas Becker <jonashbecker@gmail.com>
2020-05-20 15:05:33 +0200

    Initial Commit

---
+++ src/test/java/de/fh_kiel/oop/Dummy.java
@@ -0,0 +1,12 @@
+package de.fh_kiel.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class Dummy {
+    @Test
+    public void theGreenCheckTest() {
+		// This Test does nothing! :)
+    }
+}
